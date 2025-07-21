2803902f757cd90a3e9a790c8d3642a0da15d54d
Malika Nazarova <nazarovaa.malika@gmail.com>
2025-07-21 20:01:48 +0600

    log File

---
+++ log.py
@@ -0,0 +1,10 @@
+"""Lesson 1 log notebook."""
+
+#
+# 19/07
+#
+# 1) I have finished my INTRO tasks
+# 2) Fixed all mistakes with Cursor
+# 3) Fixed mistakes on my terminal in Cursor
+# 4) Downloaded float8 and pylint
+# 5) Checked what is Pydocstyle rules
